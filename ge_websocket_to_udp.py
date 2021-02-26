3dc5e593616a64828d98aa177c5e9142468996e3
Bruce Pennypacker <bruce@pennypacker.org>
2021-02-25 20:10:23 -0500

    rewrite as a class

--- ge_websocket_to_udp.py
+++ ge_websocket_to_udp.py
@@ -47,89 +47,86 @@ machine_type = {
 }
 
 
-sleeper = None
-client = None
+class GEWebsocketToUDP:
+    def __init__(self):
+        self.sleeper = None
+        self.client = None
+        self.config = None
 
-async def log_state_change(data: Tuple[GeAppliance, Dict[ErdCodeType, Any]]):
-    """Send state changes via UDP if desireable"""
-    appliance, state_changes = data
-    if not appliance.appliance_type in machine_type:
-        return
+    async def log_state_change(self, data: Tuple[GeAppliance, Dict[ErdCodeType, Any]]):
+        """Send state changes via UDP if desireable"""
+        appliance, state_changes = data
+        if not appliance.appliance_type in machine_type:
+            return
 
-    machine = machine_type[appliance.appliance_type]
+        machine = machine_type[appliance.appliance_type]
 
-    config = configparser.ConfigParser()
-    config.read('ge_websocket_to_udp.ini')
+        if not (machine in self.config and self.config[machine]['enabled']):
+            return
 
-    if not (machine in config and config[machine]['enabled']):
-        return
+        # state_changes['0x2000'] is machine status
+        if not '0x2000' in state_changes:
+            return
 
-    # state_changes['0x2000'] is machine status
-    if not '0x2000' in state_changes:
-        return
+        b = int.from_bytes(state_changes['0x2000'], "big")
 
-    b = int.from_bytes(state_changes['0x2000'], "big")
+        ip = socket.gethostbyname(self.config[machine]['host'])
 
-    ip = socket.gethostbyname(config[machine]['host'])
+        if 'prefix' in self.config[machine]:
+            msg = "{}{}".format(self.config[machine]['prefix'], machine_status[b])
+        else:
+            msg = machine_status[b]
 
-    if 'prefix' in config[machine]:
-        msg = "{}{}".format(config[machine]['prefix'], machine_status[b])
-    else:
-        msg = machine_status[b]
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(bytes(msg, 'utf-8'), (ip, int(self.config[machine]['port'])))
 
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.sendto(bytes(msg, 'utf-8'), (ip, int(config[machine]['port'])))
+        t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-    t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        print ("{} : {} {}:{} {}".format(t, machine, self.config[machine]['host'], self.config[machine]['port'], msg))
 
-    print ("{} : {} {}:{} {}".format(t, machine, config[machine]['host'], config[machine]['port'], msg))
+    async def do_event_disconnect(self, appliance: GeAppliance):
+        print ("Received disconnect...")
+        self.client.disconnect()
+        self.sleeper.cancel_all()
+        await self.sleeper(10)
 
-async def do_event_disconnect(appliance: GeAppliance):
-    global sleeper
-    global client
-    print ("Received disconnect...")
-    client.disconnect()
-    sleeper.cancel_all()
-    await sleeper(10)
+    async def main(self, loop):
+        self.config = configparser.ConfigParser()
+        self.config.read('ge_websocket_to_udp.ini')
+        self.client = GeWebsocketClient(loop, self.config['auth']['username'], self.config['auth']['password'])
+        self.client.add_event_handler(EVENT_APPLIANCE_STATE_CHANGE, self.log_state_change)
+        self.client.add_event_handler(EVENT_DISCONNECTED, self.do_event_disconnect)
 
-async def main(loop):
-    global sleeper
-    global client
-    config = configparser.ConfigParser()
-    config.read('ge_websocket_to_udp.ini')
-    client = GeWebsocketClient(loop, config['auth']['username'], config['auth']['password'])
-    client.add_event_handler(EVENT_APPLIANCE_STATE_CHANGE, log_state_change)
-    client.add_event_handler(EVENT_DISCONNECTED, do_event_disconnect)
+        session = aiohttp.ClientSession()
 
-    session = aiohttp.ClientSession()
+        asyncio.ensure_future(self.client.async_get_credentials_and_run(session), loop=loop)
+        await self.sleeper(86400)
 
-    asyncio.ensure_future(client.async_get_credentials_and_run(session), loop=loop) 
-    await sleeper(86400)
+    def make_sleep(self):
+        async def sleeper(delay, result=None, *, loop=None):
+            coro = asyncio.sleep(delay, result=result, loop=loop)
+            task = asyncio.ensure_future(coro)
+            sleeper.tasks.add(task)
+            try:
+                return await task
+            except asyncio.CancelledError:
+                return result
+            finally:
+                sleeper.tasks.remove(task)
 
-def make_sleep():
-    async def sleeper(delay, result=None, *, loop=None):
-        coro = asyncio.sleep(delay, result=result, loop=loop)
-        task = asyncio.ensure_future(coro)
-        sleeper.tasks.add(task)
-        try:
-            return await task
-        except asyncio.CancelledError:
-            return result
-        finally:
-            sleeper.tasks.remove(task)
-
-    sleeper.tasks = set()
-    sleeper.cancel_all = lambda: sum(task.cancel() for task in sleeper.tasks)
-    return sleeper
+        sleeper.tasks = set()
+        sleeper.cancel_all = lambda: sum(task.cancel() for task in sleeper.tasks)
+        self.sleeper = sleeper
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    sleeper = make_sleep()
     while True:
         try:
+            obj = GEWebsocketToUDP()
+            obj.make_sleep()
             t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             print("{}: starting async loop...".format(t))
-            loop.run_until_complete(main(loop))
+            loop.run_until_complete(obj.main(loop))
         except Exception as e:
             print("Caught exception: {}".format(e))
             pass
